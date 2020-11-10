a2161f3795d2251431fc2b65236ffab3ee9126f6
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-10 17:29:15 -0600

    app.js added (02-activities) | components added - header.js and wrapper.js

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+// renders Wrapper component
+import React from 'react';
+import "../styles/Wrapper.css";
+
+function Wrapper({ children }) {
+  return (
+      <div className="wrapper">
+        { children }
+      </div>
+  );
+}
+
+export default Wrapper;
