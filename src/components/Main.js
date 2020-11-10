fe8583fe50029dc369eee68edda8501531f67e10
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-10 17:15:21 -0600

    Main.js added (following class activities outline - 02)

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+// What happens inside of src/components
+// function returns some JSX decribing the UI to a particular component to be able to render to the document. 'npm start' would update inside of the web browser without requiring refresh.
+import React, { Component } from "react";
+import DataArea from "./DataArea";
+import "../styles/Main.css";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <DataArea />
+      </>
+    );
+  }
+}
