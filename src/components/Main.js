40b21184884943ee49026e2fab5dff69073911ea
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-10 18:23:09 -0600

    app working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,13 @@
 // What happens inside of src/components
 // function returns some JSX decribing the UI to a particular component to be able to render to the document. 'npm start' would update inside of the web browser without requiring refresh.
 import React, { Component } from "react";
-import DataArea from "./DataArea";
 import "../styles/Main.css";
 
 export default class Main extends Component {
   render() {
     return (
       <>
-        <DataArea />
+        {/* <DataArea /> */}
       </>
     );
   }
