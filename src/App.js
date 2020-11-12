88fa72c00e3168f43ee6ecbe0e133c2be3774f6f
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-11 18:31:41 -0600

    worked with tuor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // returns the Main.js component
-import React from "react";
+import React,{useState, useEffect} from "react";
+import axios from "axios";
 import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
@@ -7,14 +8,31 @@ import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
-    </div>
-  );
+
+    useEffect(() => {
+        //Display the value in the console. Initially it is empty.
+        axios.get("https://randomuser.me/api/?results=10&nat=us")
+        .then(response => {
+            console.log(response);
+        })
+    }, []);
+
+
+
+    return (
+        <div className="App">
+            <Wrapper>
+                <Header />
+                <Main />
+            </Wrapper>
+        </div>
+    );
 }
+
+
+
+
+
+
 
 export default App;
---
+++ src/components/table.js
@@ -0,0 +1,12 @@
+import React,{useState, useEffect} from "react";
+
+import {Table, Row, Col} from "react-bootstrap";
+
+const EmployeeTable = props => {
+    console.log(props);
+    return (<React.Fragment>
+        <h1> hello</h1>
+    </React.Fragment>)
+}
+
+export default EmployeeTable;
